c40d2e6f5a2c6ec9beb401740d2ba8a4a6fb26c6
til17005 <matthew_tilton@outlook.com>
2026-01-05 12:04:30 -0700

    Week 1 Excersise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,62 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("Please enter you grade percentage (0-100): ");
+        string percentage = Console.ReadLine();
+        int gradePercent = int.Parse(percentage);
+
+        string letterGrade = "";
+
+        if (gradePercent >= 90)
+        {
+            letterGrade = "A";
+            //Console.WriteLine("Your grade is an A.");
+        }
+        else if (gradePercent >= 80 && gradePercent < 90)
+        {
+            letterGrade = "B";
+            //Console.WriteLine("Your grade is a B.");
+        }
+        else if (gradePercent >= 70 && gradePercent < 80)
+        {
+            letterGrade = "C";
+            //Console.WriteLine("Your grade is a C.");
+        }
+        else if (gradePercent >= 60 && gradePercent < 70)
+        {
+            letterGrade = "D";
+            //Console.WriteLine("Your grade is a D.");
+        }
+        else
+        {
+            letterGrade = "F";
+            //Console.WriteLine("Your grade is an F.");
+        }
+
+        Console.WriteLine($"Your letter grade is {letterGrade}.");
+
+        int lastDigit = Math.Abs(gradePercent % 10);
+        string sign = "";
+
+        if (lastDigit >= 7 && gradePercent < 94 && gradePercent > 60)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3 && gradePercent > 60)
+        {
+            sign = "-";
+        }
+
+        Console.WriteLine($"Your letter grade is {letterGrade}{sign}.");
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("I'm sorry, but you have not passed this course. Don't give up. You can do this!");
+        }
     }
 }
