8b78cc01be3de67528d1dce20cfa27609ffbbce0
til17005 <matthew_tilton@outlook.com>
2026-01-05 15:20:48 -0700

    Week 1 Excercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        DisplayResult();
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string number = Console.ReadLine();
+        int favoriteNumber = int.Parse(number);
+        return favoriteNumber;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int squaredNumber = number * number;
+        return squaredNumber;
+    }
+
+    static void DisplayResult()
+    {
+        string name = PromptUserName();
+        int number = SquareNumber(PromptUserNumber());
+
+        Console.WriteLine($"{name}, the square of your favorite number is {number}."); 
     }
 }
